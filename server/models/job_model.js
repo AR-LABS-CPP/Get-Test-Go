94ae483d9e907ddaf8af2b0e4787f87730a85c27
AR-LABS-CPP <aliraza.abro.hyd@gmail.com>
2023-02-14 16:28:14 +0500

    Duplicate is checked before creating a new job

--- server/models/job_model.js
+++ server/models/job_model.js
@@ -1,5 +1,6 @@
 const pool = require("../db")
 
+const job_table_name = "get_test_go_job"
 const job_types_table_name = "get_test_go_job_types"
 
 const getJobTypes = () => {
@@ -28,6 +29,19 @@ const addNewJob = (job_name, job_details, job_type) => {
     })
 }
 
+const jobAlreadyExists = (job_name) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT COUNT(*) FROM ${job_table_name} WHERE job_name ILIKE '${job_name}'`, (error, results) => {
+            if(error) {
+                console.log(error)
+                reject(error)
+            }
+
+            resolve(results.rowCount > 0)
+        })
+    })
+}
+
 const bindJobAndAssessment = (job_name, assessment_name) => {
     return new Promise((resolve, reject) => {
         pool.query(`call add_job_assessment('${job_name}', '${assessment_name}')`, (error, results) => {
@@ -45,4 +59,5 @@ module.exports = {
     getJobTypes,
     addNewJob,
     bindJobAndAssessment,
+    jobAlreadyExists
 }
--- server/routes/JobRoute.js
+++ server/routes/JobRoute.js
@@ -12,15 +12,24 @@ jobRoute.get("/types", (_, res) => {
 })
 
 jobRoute.post("/new", (req, res) => {
-    jobModel.addNewJob(req.body.jobName, req.body.jobDetails, req.body.jobType).then(_ => {
-        for(let idx = 0; idx < req.body.requiredAssessments.length; idx++) {
-            console.log(req.body.requiredAssessments[idx])
-            jobModel.bindJobAndAssessment(req.body.jobName, req.body.requiredAssessments[idx]).catch(error => {
-                console.log(error)
+    jobModel.jobAlreadyExists(req.body.jobName).then(response => {
+        if(response === false) {
+            jobModel.addNewJob(req.body.jobName, req.body.jobDetails, req.body.jobType).then(_ => {
+                for(let idx = 0; idx < req.body.requiredAssessments.length; idx++) {
+                    console.log(req.body.requiredAssessments[idx])
+                    jobModel.bindJobAndAssessment(req.body.jobName, req.body.requiredAssessments[idx]).catch(error => {
+                        console.log(error)
+                    })
+                }
+        
+                res.status(200).send('Job created successfully')
+            }).catch(error => {
+                res.status(500).send(error)
             })
         }
-
-        res.status(200).send('Job created successfully')
+        else {
+            res.status(409).send("Job already exists")
+        }
     }).catch(error => {
         res.status(500).send(error)
     })
