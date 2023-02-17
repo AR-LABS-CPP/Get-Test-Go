96cbe5d42519f79ce521eb0d8c20325479b07d30
AR-LABS-CPP <aliraza.abro.hyd@gmail.com>
2023-02-17 18:59:20 +0500

    Changed /new endpoint to support multiple recruiters for same assessment

--- server/routes/AssessmentsRoute.js
+++ server/routes/AssessmentsRoute.js
@@ -20,36 +20,62 @@ assessmentsRouter.get("/question/types", (_, res) => {
 })
 
 assessmentsRouter.post("/new", (req, res) => {
-    assessmentModel.recruiterAssessmentExists(req.body.recruiterEmail, req.body.assessmentName).then(response => {
-        if(response === true) {
-            res.status(409).send("Assessment already exists")
+    assessmentModel.assessmentExists(req.body.assessmentName).then(response => {
+        // Response is either TRUE or FALSE
+        if(response) {
+            assessmentModel.recruiterAssessmentExists(req.body.recruiterEmail, req.body.assessmentName).then(response => {
+                // Response is either TRUE or FALSE
+                if(response) {
+                    res.status(409).send("Assessment with the same name already exists.")
+                }
+                else {
+                    assessmentModel.bindRecruiterAndAssessment(req.body.recruiterEmail, req.body.assessmentName).then(response => {
+                        res.status(200).send(response)
+                    }).catch(error => {
+                        res.status(500).send(error)
+                    })
+                }
+            }).catch(error => {
+                res.status(500).send(error)
+            })
         }
         else {
             let assessmentType = req.body.assessmentType === "GENERAL" ? 1 : 2
 
-            assessmentModel.addNewAssessment(req.body.assessmentName, req.body.assessmentDetails, assessmentType).then(_ => {
+            assessmentModel.addNewAssessment(
+                req.body.assessmentName, 
+                req.body.assessmentDetails, 
+                assessmentType
+            ).then(_ => {
                 assessmentModel.bindRecruiterAndAssessment(req.body.recruiterEmail, req.body.assessmentName).then(response => {
                     res.status(200).send(response)
                 }).catch(error => {
                     res.status(500).send(error)
                 })
             }).catch(error => {
+                console.log(error)
                 res.status(500).send(error)
             })
         }
     }).catch(error => {
         res.status(500).send(error)
     })
-})
 
-// assessmentsRouter.post("/view", (req, res) => {
     
-// })
+})
 
 assessmentsRouter.post("/question/add/mcq", (req, res) => {
     assessmentModel.assessmentQuestionExists(req.body.assessmentName, req.body.assessmentQuestion).then(response => {
         if(response === false) {
-            assessmentModel.addMCQ(req.body.assessmentName, req.body.assessmentQuestion, req.body.optionOne, req.body.optionTwo, req.body.optionThree, req.body.optionFour, req.body.correctOption).then(response => {
+            assessmentModel.addMCQ(
+                req.body.assessmentName, 
+                req.body.assessmentQuestion, 
+                req.body.optionOne, 
+                req.body.optionTwo, 
+                req.body.optionThree, 
+                req.body.optionFour, 
+                req.body.correctOption
+            ).then(response => {
                 res.status(200).send(response)
             }).catch(error => {
                 res.status(500).send(error)
