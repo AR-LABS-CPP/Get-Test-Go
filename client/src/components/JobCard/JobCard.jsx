64bf0c4f17d9d310f1f1ea59ee6b4fc7a0ba01b6
Ali Raza <def.aliraza@gmail.com>
2023-02-25 13:05:53 +0500

    Changed colors from emerald to blue

--- client/src/components/JobCard/JobCard.jsx
+++ client/src/components/JobCard/JobCard.jsx
@@ -1,13 +1,13 @@
 const JobCard = (props) => {
     return (
         <div>
-            <p className="w-full text-center text-white font-bold bg-emerald-500 rounded-tl-lg rounded-tr-lg">{props.badgeText}</p>
+            <p className="w-full text-center text-white font-bold bg-blue-400 rounded-tl-lg rounded-tr-lg">{props.badgeText}</p>
             <div className={`w-72 h-56 flex flex-col justify-around border-[1px] border-gray-300 shadow-lg h-92 rounded-bl-lg rounded-br-lg overflow-hidden ${props.additionalStyling}`}>
                 <p className="p-2 text-center text-2xl font-bold text-gray-600">
                     {props.cardTitle}
                 </p>
                 <div className="flex justify-center items-center">
-                    <button className="w-[150px] bg-emerald-600 hover:bg-emerald-500 py-2 text-white rounded-md" onClick={props.viewClickHandler}>View</button>
+                    <button className="w-[150px] bg-blue-600 hover:bg-blue-500 py-2 text-white rounded-md" onClick={props.viewClickHandler}>View</button>
                 </div>
             </div>
         </div>
