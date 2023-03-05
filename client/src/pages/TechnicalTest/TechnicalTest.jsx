6df5d562e7a75f3675c62382a36aabeaa109aefb
AR-LABS-CPP <aliraza.abro.hyd@gmail.com>
2023-03-05 17:03:18 +0500

    BUG: The next assessment only show the last question.

--- client/src/pages/TechnicalTest/TechnicalTest.jsx
+++ client/src/pages/TechnicalTest/TechnicalTest.jsx
@@ -1,48 +1,266 @@
-import { useEffect, useState } from "react"
-import QuestionBox from "../../components/QuestionBox/QuestionBox"
+// import { useEffect, useState } from "react"
+// import QuestionBox from "../../components/QuestionBox/QuestionBox"
+
+// const TechnicalTest = (props) => {
+//     const [questions, setQuestions] = useState(props.questions)
+//     const [activeQuestion, setActiveQuestion] = useState(0)
+//     const [mcqselectedOption, setMCQSelectedOption] = useState("")
+//     const [tfSelectedOption, setTFSelectedOption] = useState(true)
+//     const [buttonText, setButtonText] = useState("Next")
+//     const [errorText, setErrorText] = useState("")
+//     const assessmentInfo = {
+//         sectionName: `TECHNICAL / ${props.assessmentName}`,
+//         totalQuestions: props.totalQuestions,
+//     }
+//     const [secondsLeft, setSecondsLeft] = useState(10)
+
+//     useEffect(() => {
+//         // Every second, handleTimeOver() is called to
+//         // check if the time is over and if so
+//         // then move to the next question using this
+//         // function and mark the currently selected
+//         // answer of the user else mark NULL
+//         handleTimeOver()
+//     }, [secondsLeft])
+
+//     useEffect(() => {
+//         setSecondsLeft(10)
+
+//         if (activeQuestion === (props.questions.length - 1)) {
+//             setButtonText("Finish")
+//         }
+//         else {
+//             setButtonText("Next")
+//         }
+
+//         setMCQSelectedOption("")
+//         setTFSelectedOption(true)
+//         setErrorText("")
+
+//         let intervalId = setInterval(() => {
+//             setSecondsLeft((prevSeconds) => prevSeconds - 1);
+//         }, 1000);
+
+//         return () => clearInterval(intervalId);
+//     }, [activeQuestion])
+
+//     const formatTime = (timeInSeconds) => {
+//         const minutes = Math.floor(timeInSeconds / 60);
+//         const seconds = timeInSeconds % 60;
+//         return `${minutes.toString().padStart(2, "0")}:${seconds
+//             .toString()
+//             .padStart(2, "0")}`;
+//     };
+
+//     const getSelectedOption = () => {
+//         if(questions[activeQuestion].question_type_name === "MCQ") {
+//             return mcqselectedOption
+//         }
+//         else if(questions[activeQuestion].question_type_name === "TrueFalse") {
+//             return tfSelectedOption
+//         }
+//     }
+
+//     const handleSelectOption = (evt) => {
+//         if (questions[activeQuestion].question_type_name === "MCQ") {
+//             setMCQSelectedOption(evt.target.value)
+//         }
+//         else if (questions[activeQuestion].question_type_name === "TrueFalse") {
+//             setTFSelectedOption(evt.target.value)
+//         }
+//     }
+
+//     const handleNextQuestion = () => {
+//         if (buttonText === "Finish") {
+//             handleFinish()
+//         }
+//         else {
+//             if (questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption === "") {
+//                 setErrorText("Please select an option")
+//             }
+//             else {
+//                 if (localStorage.getItem("CANDIDATE_ANSWERS")) {
+//                     let newAnswers = JSON.parse(localStorage.getItem("CANDIDATE_ANSWERS"))
+//                     newAnswers.push(getSelectedOption())
+
+//                     localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify(newAnswers))
+
+//                     setActiveQuestion(prevValue => prevValue + 1)
+//                 }
+//                 else {
+//                     localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify([getSelectedOption()]))
+
+//                     setActiveQuestion(prevValue => prevValue + 1)
+//                 }
+//             }
+//         }
+//     }
+
+//     const handleFinish = () => {
+//         let finalAnswers = JSON.parse(localStorage.getItem("CANDIDATE_ANSWERS"))
+//         finalAnswers.push(getSelectedOption())
+
+//         localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify(finalAnswers))
+
+//         props.storeAnswersCB(props.assessmentName, finalAnswers)
+
+//         localStorage.removeItem("CANDIDATE_ANSWERS")
+
+//         props.setActiveAssessmentCB()
+//     }
+
+//     const handleTimeOver = () => {
+//         if(secondsLeft === 0) {
+//             if (localStorage.getItem("CANDIDATE_ANSWERS")) {
+//                 let newAnswers = JSON.parse(localStorage.getItem("CANDIDATE_ANSWERS"))
+
+//                 if(questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption !== "") {
+//                     newAnswers.push(mcqselectedOption)
+//                 }
+//                 else if(questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption === "") {
+//                     newAnswers.push("NULL")
+//                 }
+//                 else if(questions[activeQuestion].question_type_name === "TrueFalse") {
+//                     newAnswers.push(tfSelectedOption)
+//                 }
+
+//                 localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify(newAnswers))
+
+//                 setActiveQuestion(prevValue => prevValue + 1)
+//             }
+//             else {
+//                 let answers = []
+
+//                 if(questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption !== "") {
+//                     answers.push(mcqselectedOption)
+//                 }
+//                 else if(questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption === "") {
+//                     answers.push("NULL")
+//                 }
+//                 else if(questions[activeQuestion].question_type_name === "TrueFalse") {
+//                     answers.push(tfSelectedOption)
+//                 }
+
+//                 localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify(answers))
+
+//                 setActiveQuestion(prevValue => prevValue + 1)
+//             }
+//         }
+//     }
+
+//     return (
+//         props.visible
+//         &&
+//         <div>
+//             {/* Can make a separate component to avoid markup repitition */}
+//             <div className="m-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 grid-rows-1 border-[1px] rounded-lg shadow-md">
+//                 <div className="flex flex-col">
+//                     <p className="rounded-tr-lg rounded-tl-lg md:rounded-tr-none lg:rounded-tr-none bg-blue-500 w-full text-white text-center py-2 font-bold">Section</p>
+//                     <p className="text-center py-2 font-bold">{assessmentInfo.sectionName}</p>
+//                 </div>
+//                 <div className="flex flex-col">
+//                     <p className="bg-blue-500 w-full text-white text-center py-2 font-bold">Total Questions</p>
+//                     <p className="text-center py-2 font-bold">{assessmentInfo.totalQuestions}</p>
+//                 </div>
+//                 <div className="flex flex-col">
+//                     <p className="rounded-tr-none md:rounded-tr-lg lg:rounded-tr-lg xl:rounded-tr-lg bg-blue-500 w-full text-white text-center py-2 font-bold">Time remaining for this question</p>
+//                     <p className="text-center py-2 font-bold">{formatTime(secondsLeft)}</p>
+//                 </div>
+//             </div>
+
+//             <div className="w-full text-center">
+//                 <span className="text-sm text-red-500">{errorText}</span>
+//             </div>
+
+//             {
+//                 questions.map((q, idx) => {
+//                     if (q.question_type_name === "MCQ") {
+//                         return <QuestionBox
+//                             key={q + idx}
+//                             visible={idx === activeQuestion}
+//                             mcq={true}
+//                             truefalse={false}
+//                             question={q.question}
+//                             optionOne={q.option_one}
+//                             optionTwo={q.option_two}
+//                             optionThree={q.option_three}
+//                             optionFour={q.option_four}
+//                             handleQuestionOptionClick={handleSelectOption}
+//                         />
+//                     }
+//                     else {
+//                         return <QuestionBox
+//                             key={q + idx}
+//                             visible={idx === activeQuestion}
+//                             mcq={false}
+//                             truefalse={true}
+//                             question={q.question}
+//                             handleQuestionOptionClick={handleSelectOption}
+//                         />
+//                     }
+//                 })
+//             }
+
+//             <div className="w-full flex justify-center gap-x-3 text-white">
+//                 <button onClick={handleNextQuestion} className="bg-blue-600 hover:bg-blue-500 px-10 py-2 rounded-md">
+//                     {
+//                         buttonText
+//                     }
+//                 </button>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default TechnicalTest
+
+import { useEffect, useState, useRef } from "react";
+import QuestionBox from "../../components/QuestionBox/QuestionBox";
 
 const TechnicalTest = (props) => {
-    const [questions, setQuestions] = useState(props.questions)
-    const [activeQuestion, setActiveQuestion] = useState(0)
-    const [mcqselectedOption, setMCQSelectedOption] = useState("")
-    const [tfSelectedOption, setTFSelectedOption] = useState(true)
-    const [buttonText, setButtonText] = useState("Next")
-    const [errorText, setErrorText] = useState("")
+    const [questions, setQuestions] = useState(props.questions);
+    const [activeQuestion, setActiveQuestion] = useState(0);
+    const [mcqselectedOption, setMCQSelectedOption] = useState("");
+    const [tfSelectedOption, setTFSelectedOption] = useState(true);
+    const [buttonText, setButtonText] = useState("Next");
+    const [errorText, setErrorText] = useState("");
+    const [assessmentName, setAssessmentName] = useState(props.assessmentName)
+    const [secondsLeft, setSecondsLeft] = useState(10);
+    const candidateAnswersRef = useRef([]);
     const assessmentInfo = {
         sectionName: `TECHNICAL / ${props.assessmentName}`,
         totalQuestions: props.totalQuestions,
-    }
-    const [secondsLeft, setSecondsLeft] = useState(10)
+    };
 
     useEffect(() => {
         // Every second, handleTimeOver() is called to
-        // check whether 0 seconds are left and if so
+        // check if the time is over and if so
         // then move to the next question using this
         // function and mark the currently selected
         // answer of the user else mark NULL
-        handleTimeOver()
-    }, [secondsLeft])
+        handleTimeOver();
+    }, [secondsLeft]);
 
     useEffect(() => {
-        setSecondsLeft(10)
+        setSecondsLeft(10);
 
-        if (activeQuestion === (props.questions.length - 1)) {
+        if (activeQuestion === (questions.length - 1)) {
             setButtonText("Finish")
         }
         else {
             setButtonText("Next")
         }
 
-        setMCQSelectedOption("")
-        setTFSelectedOption(true)
-        setErrorText("")
+        setMCQSelectedOption("");
+        setTFSelectedOption(true);
+        setErrorText("");
 
         let intervalId = setInterval(() => {
             setSecondsLeft((prevSeconds) => prevSeconds - 1);
         }, 1000);
 
         return () => clearInterval(intervalId);
-    }, [activeQuestion])
+    }, [activeQuestion]);
 
     const formatTime = (timeInSeconds) => {
         const minutes = Math.floor(timeInSeconds / 60);
@@ -53,101 +271,70 @@ const TechnicalTest = (props) => {
     };
 
     const getSelectedOption = () => {
-        if(questions[activeQuestion].question_type_name === "MCQ") {
-            return mcqselectedOption
+        if (questions[activeQuestion].question_type_name === "MCQ") {
+            return mcqselectedOption;
+        } else if (questions[activeQuestion].question_type_name === "TrueFalse") {
+            return tfSelectedOption;
         }
-        else if(questions[activeQuestion].question_type_name === "TrueFalse") {
-            return tfSelectedOption
-        }
-    }
+    };
 
     const handleSelectOption = (evt) => {
         if (questions[activeQuestion].question_type_name === "MCQ") {
-            setMCQSelectedOption(evt.target.value)
+            setMCQSelectedOption(evt.target.value);
+        } else if (questions[activeQuestion].question_type_name === "TrueFalse") {
+            setTFSelectedOption(evt.target.value);
         }
-        else if (questions[activeQuestion].question_type_name === "TrueFalse") {
-            setTFSelectedOption(evt.target.value)
-        }
-    }
+    };
 
     const handleNextQuestion = () => {
         if (buttonText === "Finish") {
-            handleFinish()
-        }
-        else {
-            if (questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption === "") {
-                setErrorText("Please select an option")
-            }
-            else {
-                if (localStorage.getItem("CANDIDATE_ANSWERS")) {
-                    let newAnswers = JSON.parse(localStorage.getItem("CANDIDATE_ANSWERS"))
-                    newAnswers.push(getSelectedOption())
-
-                    localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify(newAnswers))
-
-                    setActiveQuestion(prevValue => prevValue + 1)
-                }
-                else {
-                    localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify([getSelectedOption()]))
-
-                    setActiveQuestion(prevValue => prevValue + 1)
-                }
+            handleFinish();
+        } else {
+            if (
+                questions[activeQuestion].question_type_name === "MCQ" &&
+                mcqselectedOption === ""
+            ) {
+                setErrorText("Please select an option");
+            } else {
+                candidateAnswersRef.current.push(getSelectedOption());
+                setActiveQuestion((prevValue) => prevValue + 1);
             }
         }
-    }
+    };
 
     const handleFinish = () => {
-        let finalAnswers = JSON.parse(localStorage.getItem("CANDIDATE_ANSWERS"))
-        finalAnswers.push(getSelectedOption())
-
-        localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify(finalAnswers))
-
-        props.storeAnswersCB(props.assessmentName, finalAnswers)
-
-        localStorage.removeItem("CANDIDATE_ANSWERS")
-
-        props.setActiveAssessmentCB()
-    }
+        candidateAnswersRef.current.push(getSelectedOption());
+        props.storeAnswersCB(assessmentName, candidateAnswersRef.current);
+        candidateAnswersRef.current = [];
+        props.setActiveAssessmentCB();
+    };
 
     const handleTimeOver = () => {
-        if(secondsLeft === 0) {
-            if (localStorage.getItem("CANDIDATE_ANSWERS")) {
-                let newAnswers = JSON.parse(localStorage.getItem("CANDIDATE_ANSWERS"))
-                
-                if(questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption !== "") {
-                    newAnswers.push(mcqselectedOption)
-                }
-                else if(questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption === "") {
-                    newAnswers.push("NULL")
-                }
-                else if(questions[activeQuestion].question_type_name === "TrueFalse") {
-                    newAnswers.push(tfSelectedOption)
-                }
-    
-                localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify(newAnswers))
-    
-                setActiveQuestion(prevValue => prevValue + 1)
+        if (secondsLeft === 0) {
+            if(buttonText === "Finish") {
+                handleFinish()
             }
             else {
-                let answers = []
-    
-                if(questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption !== "") {
-                    answers.push(mcqselectedOption)
+                if (
+                    questions[activeQuestion].question_type_name === "MCQ" &&
+                    mcqselectedOption !== ""
+                ) {
+                    candidateAnswersRef.current.push(getSelectedOption());
+                } else if (
+                    questions[activeQuestion].question_type_name === "TrueFalse"
+                ) {
+                    candidateAnswersRef.current.push(getSelectedOption());
+                } else {
+                    candidateAnswersRef.current.push(null);
                 }
-                else if(questions[activeQuestion].question_type_name === "MCQ" && mcqselectedOption === "") {
-                    answers.push("NULL")
-                }
-                else if(questions[activeQuestion].question_type_name === "TrueFalse") {
-                    answers.push(tfSelectedOption)
-                }
     
-                localStorage.setItem("CANDIDATE_ANSWERS", JSON.stringify(answers))
+                console.log(candidateAnswersRef.current)
     
-                setActiveQuestion(prevValue => prevValue + 1)
+                setActiveQuestion((prevValue) => prevValue + 1);
             }
         }
-    }
-    
+    };
+
     return (
         props.visible
         &&
@@ -188,7 +375,7 @@ const TechnicalTest = (props) => {
                             handleQuestionOptionClick={handleSelectOption}
                         />
                     }
-                    else {
+                    else if(q.question_type_name === "TrueFalse") {
                         return <QuestionBox
                             key={q + idx}
                             visible={idx === activeQuestion}
@@ -210,6 +397,6 @@ const TechnicalTest = (props) => {
             </div>
         </div>
     )
-}
+}  
 
 export default TechnicalTest
--- client/src/pages/TechnicalTest/TechnicalTestRouter.jsx
+++ client/src/pages/TechnicalTest/TechnicalTestRouter.jsx
@@ -1,6 +1,97 @@
+// import axios from "axios"
+// import { useEffect, useState } from "react"
+// import { useLocation, useNavigate } from "react-router-dom"
+// import TechnicalTest from "./TechnicalTest"
+
+// const TechnicalTestRouter = () => {
+//     const { state } = useLocation()
+
+//     const [assessments, setAssessments] = useState([])
+//     const [activeAssessment, setActiveAssessment] = useState(0)
+
+//     useEffect(() => {
+//         axios.post("http://localhost:4321/job/assessments/questions", {
+//             recruiterEmail: state.recruiterEmail,
+//             jobName: state.jobName
+//         }).then(response => {
+//             setAssessments(response.data)
+
+//             if(localStorage.getItem("CANDIDATE_ANSWERS")) {
+//                 localStorage.removeItem("CANDIDATE_ANSWERS")
+//             }
+
+//             if (localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS")) {
+//                 localStorage.removeItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS")
+//             }
+//         }).catch(error => {
+//             console.log(error)
+//         })
+//     }, [])
+
+//     const getCandidateEmail = () => {
+//         return jwt.decode(localStorage.getItem("token"))
+//             && jwt.decode(localStorage.getItem("token")).email
+//     }
+
+//     const handleNextAssessment = () => {
+//         if (activeAssessment === (assessments.length - 1)) {
+//             if (localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS")) {
+//                 axios.post("http://localhost:4321/assessment/technical/calculate_score", {
+//                     recruiterEmail: state.recruiterEmail,
+//                     candidateAnswers: JSON.parse(localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS"))
+//                 }).then(response => {
+//                     console.log(response)
+//                 }).catch(error => {
+//                     console.log(error)
+//                 })
+//             }
+//             else {
+//                 console.log("CANDIDATE ASSESSMENTS SOMEHOW GOT DELETED!")
+//             }
+//         }
+//         else {
+//             setActiveAssessment((prevValue) => prevValue + 1)
+//         }
+//     }
+
+//     const storeAnswers = (assessmentName, answers) => {
+//         if (localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS")) {
+//             let previousTestsAnswers = JSON.parse(localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS"))
+
+//             previousTestsAnswers.push([assessmentName, answers])
+
+//             localStorage.setItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS", JSON.stringify(previousTestsAnswers))
+//         }
+//         else {
+//             let testAnswers = []
+//             testAnswers.push([assessmentName, answers])
+
+//             localStorage.setItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS", JSON.stringify(testAnswers))
+//         }
+//     }
+
+//     return (
+//         assessments.length > 0 && assessments.map((assessment, idx) => {
+//             return (
+//                 <TechnicalTest
+//                     key={assessment + idx}
+//                     questions={assessment}
+//                     visible={idx === activeAssessment}
+//                     assessmentName={assessment[0].assessment_name}
+//                     totalQuestions={assessment.length}
+//                     setActiveAssessmentCB={handleNextAssessment}
+//                     storeAnswersCB={storeAnswers}
+//                 />
+//             )
+//         })
+//     )
+// }
+
+// export default TechnicalTestRouter
+
 import axios from "axios"
-import { useEffect, useState } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
+import { useEffect, useRef, useState } from "react"
+import { useLocation } from "react-router-dom"
 import TechnicalTest from "./TechnicalTest"
 
 const TechnicalTestRouter = () => {
@@ -9,6 +100,8 @@ const TechnicalTestRouter = () => {
     const [assessments, setAssessments] = useState([])
     const [activeAssessment, setActiveAssessment] = useState(0)
 
+    const candidateTechnicalTestsAnswers = useRef([])
+
     useEffect(() => {
         axios.post("http://localhost:4321/job/assessments/questions", {
             recruiterEmail: state.recruiterEmail,
@@ -16,13 +109,8 @@ const TechnicalTestRouter = () => {
         }).then(response => {
             setAssessments(response.data)
 
-            if(localStorage.getItem("CANDIDATE_ANSWERS")) {
-                localStorage.removeItem("CANDIDATE_ANSWERS")
-            }
+            candidateTechnicalTestsAnswers.current = []
 
-            if (localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS")) {
-                localStorage.removeItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS")
-            }
         }).catch(error => {
             console.log(error)
         })
@@ -35,10 +123,10 @@ const TechnicalTestRouter = () => {
 
     const handleNextAssessment = () => {
         if (activeAssessment === (assessments.length - 1)) {
-            if (localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS")) {
+            if (candidateTechnicalTestsAnswers.current.length > 0) {
                 axios.post("http://localhost:4321/assessment/technical/calculate_score", {
                     recruiterEmail: state.recruiterEmail,
-                    candidateAnswers: JSON.parse(localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS"))
+                    candidateAnswers: candidateTechnicalTestsAnswers.current
                 }).then(response => {
                     console.log(response)
                 }).catch(error => {
@@ -55,19 +143,7 @@ const TechnicalTestRouter = () => {
     }
 
     const storeAnswers = (assessmentName, answers) => {
-        if (localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS")) {
-            let previousTestsAnswers = JSON.parse(localStorage.getItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS"))
-
-            previousTestsAnswers.push([assessmentName, answers])
-
-            localStorage.setItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS", JSON.stringify(previousTestsAnswers))
-        }
-        else {
-            let testAnswers = []
-            testAnswers.push([assessmentName, answers])
-
-            localStorage.setItem("CANDIDATE_TECHNICAL_TESTS_ANSWERS", JSON.stringify(testAnswers))
-        }
+        candidateTechnicalTestsAnswers.current.push([assessmentName, answers])
     }
 
     return (
