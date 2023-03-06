65807efcbccb89538bcf0fe9985b411521e82b2c
AR-LABS-CPP <aliraza.abro.hyd@gmail.com>
2023-03-06 11:57:01 +0500

    Only timer is remaining

--- client/src/pages/TechnicalTest/TechnicalTest.jsx
+++ client/src/pages/TechnicalTest/TechnicalTest.jsx
@@ -7,10 +7,11 @@ const TechnicalTest = () => {
 
   const [error, setError] = useState("")
   const [assessments, setAssessments] = useState([])
+  const [secondsLeft, setSecondsLeft] = useState(10)
   const [activeQuestion, setActiveQuestion] = useState(0)
   const [activeAssessment, setActiveAssessment] = useState(0)
   const [activeQuestionOption, setActiveQuestionOption] = useState("")
-  const [activeAssessmentMarks, setActiveAssessmentAnswers] = useState([])
+  const [activeAssessmentAnswers, setActiveAssessmentAnswers] = useState([])
   const candidateAnswers = useRef([])
 
   useEffect(() => {
@@ -32,6 +33,32 @@ const TechnicalTest = () => {
     })
   }, [])
 
+  useEffect(() => {
+    setSecondsLeft(10)
+    setError("")
+    setActiveQuestionOption("")
+
+    let intervalId = setInterval(() => {
+      setSecondsLeft((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [activeQuestion])
+
+  useEffect(() => {
+    if (secondsLeft === 0) {
+      handleTimeFinished()
+    }
+  }, [secondsLeft])
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  }
+
   const setOption = (evt) => {
     setActiveQuestionOption(evt.target.value)
   }
@@ -39,29 +66,39 @@ const TechnicalTest = () => {
   const handleNextQuestion = () => {
     // This is the last question of the last assessment
     if (activeAssessment === (assessments.length - 1) && activeQuestion === (assessments[activeAssessment].length - 1)) {
-      if(activeQuestion === "") {
+      if (activeQuestion === "") {
         setError("Please select an option")
       }
       else {
-        // Calculate the scores
+        console.log("Technical section finished")
       }
     }
     else if (activeQuestion === assessments[activeAssessment].length - 1) {
-      if(activeQuestionOption === "") {
+      if (activeQuestionOption === "") {
         setError("Please select an option")
       }
       else {
-        candidateAnswers.current.push([assessments[activeAssessment][0].assessment_name, activeAssessmentMarks])
+        candidateAnswers.current.push([assessments[activeAssessment][0].assessment_name, activeAssessmentAnswers])
         setActiveAssessment(val => val + 1)
       }
     }
     else {
-      if(activeQuestionOption === "") {
+      if (activeQuestionOption === "") {
         setError("Please select an option")
       }
       else {
         setActiveQuestion(val => val + 1)
       }
+    }
+  }
+
+  const handleTimeFinished = () => {
+    if(activeQuestionOption === "") {
+      setActiveQuestionOption("NOT-SELECTED")
+      setActiveAssessmentAnswers([...activeAssessmentAnswers, null])
+    }
+    else if(activeQuestionOption !== "") {
+      setActiveAssessmentAnswers([...activeAssessmentAnswers, activeQuestionOption])
     }
   }
 
@@ -77,6 +114,7 @@ const TechnicalTest = () => {
                 assessmentIndex === activeAssessment && questionIndex === activeQuestion
                 &&
                 <div>
+                  <div>{formatTime(secondsLeft)}</div>
                   <span className="text-red-500 text-sm">{error}</span>
                   <div className="m-5 flex gap-y-4 flex-col border-[1px] rounded-lg shadow-md">
                     <p className="p-5 border-[1px] m-4 rounded-lg text-center">{q.question}</p>
@@ -100,10 +138,10 @@ const TechnicalTest = () => {
                   </div>
 
                   <div className="w-full flex justify-center gap-x-3 text-white">
-                <button onClick={handleNextQuestion} className="bg-blue-600 hover:bg-blue-500 px-10 py-2 rounded-md">
-                    { questionIndex === assessment.length ? "Finish" : "Next"}
-                </button>
-            </div>
+                    <button onClick={handleNextQuestion} className="bg-blue-600 hover:bg-blue-500 px-10 py-2 rounded-md">
+                      {questionIndex === assessment.length ? "Finish" : "Next"}
+                    </button>
+                  </div>
                 </div>
               )
             })
